c38adaa6f5b81d208dcdde428c60c8f128957b57
Android Developer Community STMIK IKMI Cirebon <119995656+adcomofficial@users.noreply.github.com>
2023-05-13 16:57:28 +0700

    Create learn.js
    Tugas Pertemuan 13 Mei 2023.
    
    Perkenalan Express JS dengan proses routing

---
+++ todo/learn.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * Buatlah 5 endpoint dengan keterangan sebagai berikut :
+ *
+ *  1.) index ('/')
+ *  Status 200
+ *  Menampilkan responses 'Hai ini dari endpoint index'
+ *
+ *  2.) create ('/create')
+ *  Status 201
+ *  Menampilkan responses 'Hai ini dari endpoint create'
+ *
+ *  3.) edit ('/edit')
+ *  Status 200
+ *  Menampilkan responses 'Hai ini dari endpoint edit'
+ *
+ *  4.) delete ('/delete')
+ *  Status 200
+ *  Menampilkan responses 'Hai ini dari endpoint delete'
+ *
+ *  5. not found ('/not-found')
+ *  Status 404
+ *  Menampilkan responses 'Terjadi kesalahan routes tidak ditemukan'
+ *
+ *  5. bad request ('/bad-request')
+ *  Status 400
+ *  Menampilkan responses 'Terjadi kesalahan bad request'
+ *
+ */
+
+// Example Responses
+{
+  status: 200;
+  message: "This if from example routes";
+}
